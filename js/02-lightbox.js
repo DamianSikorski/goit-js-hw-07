45abcb995c9a8fc0e280685fe3392317f62a2c37
DamianSikorski <damian.sikorski671@gmail.com>
2022-11-08 20:14:26 +0100

    drugie zadanie bez captions

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const galleryList = document.querySelector(".gallery");
+
+for (const item of galleryItems) {
+  const li = document.createElement("li");
+
+  const link = document.createElement("a");
+  link.classList.add("gallery__item");
+  link.href = item.original;
+
+  const img = document.createElement("img");
+  img.classList.add("gallery__image");
+  img.src = item.preview;
+  img.alt = item.description;
+
+  li.appendChild(link);
+  link.appendChild(img);
+  galleryList.appendChild(li);
+}
+
 console.log(galleryItems);
+
+let gallery = new SimpleLightbox(".gallery a");
+gallery.on("show.simplelightbox", function (e) {});
+
+console.log(SimpleLightbox);
